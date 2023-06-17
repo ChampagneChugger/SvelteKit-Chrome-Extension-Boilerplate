680fcc8f127638434344b09e8664626550351a3b
Mario Fučkan <mario.fucchan15@gmail.com>
2023-06-17 10:27:51 +0200

    SvelteKit Chrome Extension Boilerplate

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let name: any
+
+	async function hmm() {
+		const res = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+		name = await res.json()
+	}
+</script>
+
+<button on:click={hmm}>???</button>
+
+{#if name}
+	<p>ID: {name.id}</p>
+	<p>Title: {name.title}</p>
+	<p>Body: {name.body}</p>
+{/if}
